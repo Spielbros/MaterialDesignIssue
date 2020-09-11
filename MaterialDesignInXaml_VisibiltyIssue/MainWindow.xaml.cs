8c3053c72e43253360aaeeff172784b0d3618201
Felix Scherer <Scherer@zeta.local>
2020-09-11 15:43:48 +0200

    First draft of demo application

---
+++ MaterialDesignInXaml_VisibiltyIssue/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MaterialDesignInXaml_VisibiltyIssue
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            TestLabel.Visibility = (TestLabel.Visibility == Visibility.Visible) ?
+                                        Visibility.Collapsed
+                                        : Visibility.Visible;
+        }
+    }
+}
